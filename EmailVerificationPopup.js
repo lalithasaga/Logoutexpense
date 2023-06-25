373c90f7472e15c58d2d2a68cb66cc4f4c1f0388
lalithasaga <132065591+lalithasaga@users.noreply.github.com>
2023-06-25 13:47:00 +0530

    Add files via upload

---
+++ EmailVerificationPopup.js
@@ -0,0 +1,79 @@
+/*import React, { useContext } from 'react';
+import { AuthContext } from './AuthContext';
+import firebase from 'firebase/compat/app';
+import 'firebase/compat/auth';
+
+const EmailVerificationPopup = () => {
+  const { isLoggedIn, isEmailVerified } = useContext(AuthContext);
+
+  const sendVerificationEmail = () => {
+    const user = firebase.auth().currentUser;
+
+    user.sendEmailVerification()
+      .then(() => {
+        console.log('Verification email sent.');
+      })
+      .catch((error) => {
+        console.error('Error sending verification email:', error);
+      });
+  };
+
+  return (
+    <div className="popup">
+      {isLoggedIn && !isEmailVerified && (
+        <>
+          <p>Please verify your email address.</p>
+          <button onClick={sendVerificationEmail}>Verify Email</button>
+        </>
+      )}
+      {isLoggedIn && isEmailVerified && (
+        <p>Your email is now verified.</p>
+      )}
+    </div>
+  );
+};
+
+export default EmailVerificationPopup;*/
+
+
+import React, { useContext } from 'react';
+import { AuthContext } from './AuthContext';
+import firebase from 'firebase/compat/app';
+import 'firebase/compat/auth';
+
+const EmailVerificationPopup = () => {
+  const { isLoggedIn, isEmailVerified } = useContext(AuthContext);
+
+  const sendVerificationEmail = () => {
+    const user = firebase.auth().currentUser;
+  
+    if (user) {
+      user.sendEmailVerification()
+        .then(() => {
+          console.log('Verification email sent.');
+        })
+        .catch((error) => {
+          console.error('Error sending verification email:', error);
+        });
+    } else {
+      console.error('User is not authenticated.');
+    }
+  };
+  
+  return (
+    <div className="popup">
+      {isLoggedIn && !isEmailVerified && (
+        <>
+          <p>Please verify your email address.</p>
+          <button onClick={sendVerificationEmail}>Verify Email</button>
+        </>
+      )}
+      {isLoggedIn && isEmailVerified && (
+        <p>Your email is now verified.</p>
+      )}
+    </div>
+  );
+};
+
+export default EmailVerificationPopup;
+
